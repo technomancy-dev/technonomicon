3eaa2e3ca9168f0b29ab9c387abadb47d106f57c
Levi <levi@technomancy.dev>
2024-03-04 01:42:03 +0100

    create articles collection, remove cards and other boilerplate

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const articlesCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+    })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+    articles: articlesCollection,
+};
