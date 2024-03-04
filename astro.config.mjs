1a2eb67fce26bd74273ce5264d128c85c2a6c7af
Levi <levi@technomancy.dev>
2024-03-04 01:02:42 +0100

    add mdx and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), tailwind()]
+});
