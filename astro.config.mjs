6af85b262c50134e83e65cf6d0f03d3ba80363ba
Levi <levi@technomancy.dev>
2024-03-05 14:12:14 +0100

    format project with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind()],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,13 @@
 import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const articlesCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-    })
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+  }),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-    articles: articlesCollection,
+  articles: articlesCollection,
 };
