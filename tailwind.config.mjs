9104cf409cd12a016d39412079e0fc519d0e49cf
Levi <levi@technomancy.dev>
2024-03-05 21:25:15 +0100

    finish pagination add daisy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,5 +10,9 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  daisyui: {
+    themes: ["light"],
+  },
 };
