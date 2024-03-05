a4b0baf4a0b40cef08029a84ed0a9ba219a850b8
Levi <levi@technomancy.dev>
2024-03-05 15:43:42 +0100

    ground work for issues

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,15 @@ const articlesCollection = defineCollection({
     description: z.string(),
   }),
 });
+const issuesCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+  }),
+});
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   articles: articlesCollection,
+  issues: issuesCollection
 };
