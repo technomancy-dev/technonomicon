2517140be844334562ccd6dc835657cb198a3574
Levi <levi@technomancy.dev>
2024-03-06 16:59:40 +0100

    code formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dracula",
+    },
+  },
 });
